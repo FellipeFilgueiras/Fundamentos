7316f3abe03729384448b758fa3a51a9904d8528
Fellipe Filgueiras <fellipefilgueiras@outlook.com.br>
2025-01-02 21:28:49 -0300

    Saíde de dados no terminal

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Classe Console é usada para exibir informações no prompt do comando.
+
+Console.Write("Olá sem quebra de linha"); // No final do texto não irá pular linha.
+
+Console.WriteLine("Olá com quebra de linha"); // No final do texto irá pular linha.
+
+Console.WriteLine("Olá com quebra de linha"); // No final do texto irá pular linha.
